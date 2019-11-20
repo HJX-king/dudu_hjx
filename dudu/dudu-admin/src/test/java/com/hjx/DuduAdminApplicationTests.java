85f1e179ab04020edc8273c5ca981a7c9b14a144
hjx_global <1178814406@qq.com>
2019-11-20 19:38:53 +0800

    dudu_hjx创建

---
+++ dudu/dudu-admin/src/test/java/com/hjx/DuduAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuduAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ dudu/dudu-weixin/src/test/java/com/hjx/DuduWeixinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuduWeixinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
